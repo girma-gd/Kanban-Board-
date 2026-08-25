dc8b2fbf0a3724d64cab706ab07ba26463ad22cb
girma.dg <girma1dereje@gmail.com>
2026-08-25 08:05:43 +0300

    added the first kanban board

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 function App() {
   return (
-    <div>
+    <div className="app">
       <h1>Kanban Board</h1>
+
+      <div className="board">
+
+        <div className="column">
+          <h2>Todo</h2>
+        </div>
+
+        <div className="column">
+          <h2>In Progress</h2>
+        </div>
+
+        <div className="column">
+          <h2>Done</h2>
+        </div>
+
+      </div>
     </div>
   );
 }
